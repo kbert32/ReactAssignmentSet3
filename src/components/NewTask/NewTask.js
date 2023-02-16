da03b5ab24919b0561bf19be6b0751089defa88d
kbert32 <103608624+kbert32@users.noreply.github.com>
2023-02-15 17:37:11 -0700

    15.2

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,41 +1,27 @@
-import { useState } from 'react';
+import useHttp from '../../custom-hook/use-http';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const {isLoading, error, sendRequest: postTasks} = useHttp();
 
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-customhooks-tasks-be642-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-      console.log(data);
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+  
+  function enterTaskHandler(taskText) {
+    function transformTasks(tasksObj) {
+        const generatedId = tasksObj.name; // firebase-specific => "name" contains generated id
+        const createdTask = { id: generatedId, text: taskText };
+  
+        props.onAddTask(createdTask);
+    };
+    postTasks({
+      url: 'https://react-customhooks-tasks-be642-default-rtdb.firebaseio.com/tasks.json',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({text: taskText}),  
+    }, transformTasks);
   };
 
   return (
