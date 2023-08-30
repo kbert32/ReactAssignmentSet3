35882d79b7357304dd76b761f467a2ddc76f650e
kbert32 <103608624+kbert32@users.noreply.github.com>
2023-08-30 16:05:24 -0600

    fixed cart adding

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,48 +1,53 @@
-import { useContext, useState } from 'react';
+import { useContext, useState } from "react";
 
-import Modal from '../UI/Modal';
-import CartItem from './CartItem';
-import classes from './Cart.module.css';
-import CartContext from '../../store/cart-context';
-import Checkout from '../Checkout/Checkout';
-import useHttp from '../customHooks/useHttp';
+import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import Checkout from "../Checkout/Checkout";
+import useHttp from "../customHooks/useHttp";
 
 const Cart = (props) => {
   const [order, setOrder] = useState(false);
   const [didSubmit, setdidSubmit] = useState(false);
   const cartCtx = useContext(CartContext);
-  const {isLoading, error, sendRequest: sendOrder} = useHttp();
+  const { isLoading, error, sendRequest: sendOrder } = useHttp();
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
   function openOrderForm() {
     setOrder(true);
-  };
+  }
 
   const cartItemRemoveHandler = (id) => {
     cartCtx.removeItem(id);
   };
 
   const cartItemAddHandler = (item) => {
-    cartCtx.addItem(item);
-  }
+    cartCtx.addItem({
+      id: item.id,
+      name: item.name,
+      amount: 1,
+      price: item.price,
+    });
+  };
 
   function orderSubmitHandler(orderObj) {
     sendOrder({
-      url: 'https://food-order-app-57817-default-rtdb.firebaseio.com/orders.json', 
-      method: 'POST',
+      url: "https://food-order-app-57817-default-rtdb.firebaseio.com/orders.json",
+      method: "POST",
       headers: {
-          'Content-Type': 'application/json'
-        },
+        "Content-Type": "application/json",
+      },
       body: orderObj,
     });
 
     setdidSubmit(true);
-  };
-  
+  }
+
   const cartItems = (
-    <ul className={classes['cart-items']}>
+    <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
         <CartItem
           key={item.id}
@@ -63,13 +68,23 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {order && <div><Checkout onCancel={props.onClose} orderSubmit={orderSubmitHandler} /></div>}
-      {!order && <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button} onClick={openOrderForm} >Order</button>}
-      </div>}
+      {order && (
+        <div>
+          <Checkout onCancel={props.onClose} orderSubmit={orderSubmitHandler} />
+        </div>
+      )}
+      {!order && (
+        <div className={classes.actions}>
+          <button className={classes["button--alt"]} onClick={props.onClose}>
+            Close
+          </button>
+          {hasItems && (
+            <button className={classes.button} onClick={openOrderForm}>
+              Order
+            </button>
+          )}
+        </div>
+      )}
     </>
   );
 
@@ -79,7 +94,9 @@ const Cart = (props) => {
     <>
       <p className={classes.messages}>Your order has been sent!</p>
       <div className={classes.actions}>
-        <button className={classes.button} onClick={props.onClose}>Close</button>
+        <button className={classes.button} onClick={props.onClose}>
+          Close
+        </button>
       </div>
     </>
   );
@@ -88,7 +105,9 @@ const Cart = (props) => {
     <>
       <p className={classes.messages}>Error: {error}</p>
       <div className={classes.actions}>
-        <button className={classes.button} onClick={props.onClose}>Close</button>
+        <button className={classes.button} onClick={props.onClose}>
+          Close
+        </button>
       </div>
     </>
   );
